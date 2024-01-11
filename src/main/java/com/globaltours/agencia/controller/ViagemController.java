e88125f5a28fbde45575fcd7605ec35689e4cae7
Alysson <99299491+alynh0@users.noreply.github.com>
2024-01-10 22:15:37 -0300

    backend

--- src/main/java/com/globaltours/agencia/controller/ViagemController.java
+++ src/main/java/com/globaltours/agencia/controller/ViagemController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.globaltours.agencia.model.Viagem;
 import com.globaltours.agencia.service.ComentarioService;
 import com.globaltours.agencia.service.ViagemService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/viagens")
 public class ViagemController {
